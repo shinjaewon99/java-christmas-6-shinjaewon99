6c345951a6d01314f6284ff1aa9b007b1538f7d2
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-10 23:24:45 +0900

    feat : 입력한 메뉴가 메뉴판에 존재하는지에 대한 예외 기능 추가

--- src/main/java/christmas/domain/OrderStore.java
+++ src/main/java/christmas/domain/OrderStore.java
@@ -1,20 +1,35 @@
 package christmas.domain;
 
+import christmas.menu.Order;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class OrderStore {
     private static final String INPUT_BLANK_EXCEPTION_MESSAGE = "[ERROR] 공백 없이 입력해야 합니다. 다시 입력해 주세요.";
     private static final String MENU_EXCEPTION_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private final List<String> menuStore = new ArrayList<>();
 
     public void createMenuOrder(String inputMenu) {
         validateMenuForm(inputMenu);
         String[] menuOrders = inputMenu.split(",");
         for (String menuOrder : menuOrders) {
-
+            String[] menuDetails = menuOrder.split("-");
+            validateMenu(menuDetails);
         }
     }
 
     private void validateMenuForm(String inputMenu) {
         validateInputBlank(inputMenu);
         validateMenuFormat(inputMenu);
+    }
+
+    private void validateMenu(String[] menuDetails) {
+        for (int i = 0; i < menuDetails.length; i += 2) {
+            validateMenuExist(menuDetails[i]);
+            menuStore.add(menuDetails[i]);
+        }
     }
 
     private void validateInputBlank(String inputMenu) {
@@ -25,5 +40,19 @@ public class OrderStore {
     private static void validateMenuFormat(String inputMenu) {
         if (!inputMenu.contains("-"))
             throw new IllegalArgumentException(MENU_EXCEPTION_MESSAGE);
+    }
+
+    private void validateMenuExist(String inputMenu) {
+        if (!isMenuExist(inputMenu))
+            throw new IllegalArgumentException(MENU_EXCEPTION_MESSAGE);
+    }
+
+    private boolean isMenuExist(String inputMenu) {
+        return Arrays.stream(Order.values())
+                .anyMatch(order -> order.getName().equals(inputMenu));
+    }
+
+    public List<String> getMenuStore() {
+        return menuStore;
     }
 }
