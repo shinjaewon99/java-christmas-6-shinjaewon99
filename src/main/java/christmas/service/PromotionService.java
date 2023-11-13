4be49a0cb1f32cde2fdf3b4816cb805a08bbac7e
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-13 17:17:39 +0900

    feat : 할인전 금액을 구하는 기능 추가

--- src/main/java/christmas/service/PromotionService.java
+++ src/main/java/christmas/service/PromotionService.java
@@ -2,10 +2,19 @@ package christmas.service;
 
 import christmas.domain.Customer;
 import christmas.domain.OrderStore;
+import christmas.menu.Order;
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.List;
 
 public class PromotionService {
+    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance();
     private final OrderStore orderStore = new OrderStore();
+    private final BenefitDetails benefitDetails = new BenefitDetails();
+
     private Customer customer;
+    private int beforeTotalMoney;
 
     public void createVisitDate(String inputDate) {
         customer = new Customer(inputDate);
@@ -13,5 +22,45 @@ public class PromotionService {
 
     public void createMenuOrder(String inputMenu) {
         orderStore.createMenuOrder(inputMenu);
+    }
+
+    public void checkMinimumTotalMoney() {
+        if (isMinimumTotalMoney()) {
+
+        }
+    }
+
+    public boolean isMinimumTotalMoney() {
+        return benefitDetails.minimumTotalMoney(beforeTotalMoney);
+    }
+
+    public String createBeforeTotalMoney() {
+        for (int i = 0; i < getMenuSize(); i++) {
+            beforeTotalMoney += findMenuName(getMenuStore().get(i), getMenuCountStore().get(i));
+        }
+        return formatDiscount(beforeTotalMoney);
+    }
+
+    public int getMenuSize() {
+        return getMenuCountStore().size();
+    }
+
+    public List<String> getMenuStore() {
+        return orderStore.getMenuStore();
+    }
+
+    public List<Integer> getMenuCountStore() {
+        return orderStore.getMenuCountStore();
+    }
+
+    private int findMenuName(String menu, int count) {
+        return Arrays.stream(Order.values())
+                .filter(order -> order.getName().equals(menu))
+                .mapToInt(order -> order.getPrice() * count)
+                .sum();
+    }
+
+    private String formatDiscount(int discount) {
+        return NUMBER_FORMAT.format(discount);
     }
 }
