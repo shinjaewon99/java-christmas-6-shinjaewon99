7e257e5c3ef3f114aad69a62bb26cc2c2fd4de91
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-13 09:55:49 +0900

    feat : 평일 (일요일 ~ 목요일) 디저트 메뉴 할인하는 기능 추가

--- src/main/java/christmas/service/BenefitDetails.java
+++ src/main/java/christmas/service/BenefitDetails.java
@@ -1,11 +1,15 @@
 package christmas.service;
 
+import christmas.menu.OrderType;
+
 public class BenefitDetails {
     private static final int MINIMUM_ORDER_MONEY = 10_000;
     private static final int BEGIN_DISCOUNT_MONEY = 1_000;
     private static final int INCREASE_DISCOUNT_MONEY = 100;
     private static final int CHRISTMAS_DATE = 25;
+    private static final int DISCOUNT_MONEY = 2023;
     private int dDayDiscount;
+    private int weekdayDiscount;
 
     public boolean minimumTotalMoney(int beforeTotalMoney) {
         return beforeTotalMoney >= MINIMUM_ORDER_MONEY;
@@ -16,5 +20,20 @@ public class BenefitDetails {
             dDayDiscount += BEGIN_DISCOUNT_MONEY + (INCREASE_DISCOUNT_MONEY * (date - 1));
         }
         return dDayDiscount;
+    }
+
+    public int calculatedWeekdayDiscount(String menu, int count) {
+        for (OrderType type : OrderType.values()) {
+            if (type.getName().equals(menu) && type.getMenuType().equals("DESSERT")) {
+                for (int i = 0; i < count; i++) {
+                    weekdayDiscount += DISCOUNT_MONEY;
+                }
+            }
+        }
+        return weekdayDiscount;
+    }
+
+    public boolean checkWeekday(int date) {
+        return !(date % 7 == 1 || date % 7 == 2);
     }
 }
