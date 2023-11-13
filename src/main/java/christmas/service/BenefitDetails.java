8066390ef722925ca967130159f5384d2c69d08e
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-13 09:49:16 +0900

    feat : 크리스마스 디데이 할인 기능 추가

--- src/main/java/christmas/service/BenefitDetails.java
+++ src/main/java/christmas/service/BenefitDetails.java
@@ -2,8 +2,19 @@ package christmas.service;
 
 public class BenefitDetails {
     private static final int MINIMUM_ORDER_MONEY = 10_000;
+    private static final int BEGIN_DISCOUNT_MONEY = 1_000;
+    private static final int INCREASE_DISCOUNT_MONEY = 100;
+    private static final int CHRISTMAS_DATE = 25;
+    private int dDayDiscount;
 
     public boolean minimumTotalMoney(int beforeTotalMoney) {
         return beforeTotalMoney >= MINIMUM_ORDER_MONEY;
+    }
+
+    public int calculatedDayDiscount(int date) {
+        if (date <= CHRISTMAS_DATE) {
+            dDayDiscount += BEGIN_DISCOUNT_MONEY + (INCREASE_DISCOUNT_MONEY * (date - 1));
+        }
+        return dDayDiscount;
     }
 }
