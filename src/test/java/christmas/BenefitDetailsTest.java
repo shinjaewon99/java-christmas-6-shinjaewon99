918c8322f8ad438b47dc2a96b339def5b5bf8e09
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-13 10:53:18 +0900

    test : 총주문 금액, 크리스마스 디데이 할인, 디저트 메뉴 할인, 메인 메뉴 할인 기능 테스트 추가

---
+++ src/test/java/christmas/BenefitDetailsTest.java
@@ -0,0 +1,63 @@
+package christmas;
+
+import christmas.service.BenefitDetails;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BenefitDetailsTest {
+    BenefitDetails benefitDetails = new BenefitDetails();
+
+    @DisplayName("총 주문 금액이 10_000원 이상인지 확인")
+    @ValueSource(ints = {10_000, 35_000})
+    @ParameterizedTest
+    void over_Before_TotalMoney(int beforeTotalMoney) {
+        assertThat(true).isEqualTo(benefitDetails.minimumTotalMoney(beforeTotalMoney));
+    }
+
+    @DisplayName("총 주문 금액이 10_000원 보다 작은지 확인")
+    @ValueSource(ints = {9_999, 5_000})
+    @ParameterizedTest
+    void under_Before_TotalMoney(int beforeTotalMoney) {
+        assertThat(false).isEqualTo(benefitDetails.minimumTotalMoney(beforeTotalMoney));
+    }
+
+    @DisplayName("크리스마스 디데이 할인 (25일엔 3400원 할인)")
+    @Test
+    void apply_dDay_Discount() {
+        assertThat(3400).isEqualTo(benefitDetails.calculatedDayDiscount(25));
+    }
+
+    @DisplayName("크리스마스 디데이 할인 (26일엔 0원 할인)")
+    @Test
+    void not_Apply_dDay_Discount() {
+        assertThat(0).isEqualTo(benefitDetails.calculatedDayDiscount(26));
+    }
+
+    @DisplayName("디저트 메뉴를 메뉴 1개당 2_023원 할인")
+    @Test
+    void apply_Dessert_Discount() {
+        assertThat(10_115).isEqualTo(benefitDetails.calculatedWeekdayDiscount("초코케이크", 5));
+    }
+
+    @DisplayName("디저트 메뉴가 아닌경우 0원 할인")
+    @Test
+    void not_Apply_Dessert_Discount() {
+        assertThat(0).isEqualTo(benefitDetails.calculatedWeekdayDiscount("티본스테이크", 5));
+    }
+
+    @DisplayName("메인 메뉴를 메뉴 1개당 2_023원 할인")
+    @Test
+    void apply_Main_Discount() {
+        assertThat(10_115).isEqualTo(benefitDetails.calculatedWeekendDiscount("티본스테이크", 5));
+    }
+
+    @DisplayName("메인 메뉴가 아닌경우 0원 할인")
+    @Test
+    void not_Apply_Main_Discount() {
+        assertThat(0).isEqualTo(benefitDetails.calculatedWeekendDiscount("시저샐러드", 5));
+    }
+}
