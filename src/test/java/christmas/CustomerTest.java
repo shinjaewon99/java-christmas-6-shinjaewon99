bb2fbdad398cf10b801d4581b4ed80ba663f1716
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-13 11:15:00 +0900

    test : 전체 테스트 메서드명 수정

--- src/test/java/christmas/CustomerTest.java
+++ src/test/java/christmas/CustomerTest.java
@@ -11,7 +11,7 @@ class CustomerTest {
     @DisplayName("숫자가 아닌 방문 날짜 입력시 예외가 발생한다.")
     @ValueSource(strings = {"a", "12a", "a1"})
     @ParameterizedTest
-    void Invalid_DateNumber_Type(String date) {
+    void invalid_DateNumber_Type(String date) {
         assertThatThrownBy(() -> new Customer(date))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -19,7 +19,7 @@ class CustomerTest {
     @DisplayName("방문 날짜가 1 이상 31 이하가 아닌 경우 예외가 발생한다.")
     @ValueSource(strings = {"0", "32", "54"})
     @ParameterizedTest
-    void Invalid_DateNumber_Not_Exist(String date) {
+    void invalid_DateNumber_Not_Exist(String date) {
         assertThatThrownBy(() -> new Customer(date))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -27,7 +27,7 @@ class CustomerTest {
     @DisplayName("방문 날짜 입력시 공백이 존재하면 예외가 발생한다.")
     @ValueSource(strings = {" 1", "3 1", "", " "})
     @ParameterizedTest
-    void Invalid_DateNumber_Blank(String date) {
+    void invalid_DateNumber_Blank(String date) {
         assertThatThrownBy(() -> new Customer(date))
                 .isInstanceOf(IllegalArgumentException.class);
     }
--- src/test/java/christmas/OrderStoreTest.java
+++ src/test/java/christmas/OrderStoreTest.java
@@ -13,7 +13,7 @@ class OrderStoreTest {
     @DisplayName("메뉴판에 없는 메뉴를 입력하는 경우 예외가 발생한다.")
     @ValueSource(strings = {"콜라-1, 초코케이크-1,아이스크림-5", "레드와인-5,스테이크-4", "감자샐러드-4"})
     @ParameterizedTest
-    void Invalid_Menu_Not_Exist(String menu) {
+    void invalid_Menu_Not_Exist(String menu) {
         assertThatThrownBy(() -> orderStore.createMenuOrder(menu))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -21,7 +21,7 @@ class OrderStoreTest {
     @DisplayName("메뉴의 개수가 1 이상이 아닌 경우 예외가 발생한다.")
     @ValueSource(strings = {"제로콜라-0, 초코케이크-1,아이스크림-5", "레드와인-5,티본스테이크-0", "시저샐러드-0"})
     @ParameterizedTest
-    void Invalid_Menu_Count(String menu) {
+    void invalid_Menu_Count(String menu) {
         assertThatThrownBy(() -> orderStore.createMenuOrder(menu))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -29,7 +29,7 @@ class OrderStoreTest {
     @DisplayName("메뉴의 형식이 예시와 다른 경우 예외가 발생한다.")
     @ValueSource(strings = {"제로콜라 -3, 초코케이크-1,아이스크림-5", "레드 와인-5,티본스테이크-4", "샴페인4", "제로콜라-a", "", " "})
     @ParameterizedTest
-    void Invalid_Menu_Form(String menu) {
+    void invalid_Menu_Form(String menu) {
         assertThatThrownBy(() -> orderStore.createMenuOrder(menu))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -37,7 +37,7 @@ class OrderStoreTest {
     @DisplayName("중복 메뉴를 입력한 경우 예외가 발생한다.")
     @ValueSource(strings = {"시저샐러드-1,시저샐러드-1", "초코케이크-4,아이스크림-2,초코케이크-5"})
     @ParameterizedTest
-    void Invalid_Menu_Duplicate(String menu) {
+    void invalid_Menu_Duplicate(String menu) {
         assertThatThrownBy(() -> orderStore.createMenuOrder(menu))
                 .isInstanceOf(IllegalArgumentException.class);
     }
