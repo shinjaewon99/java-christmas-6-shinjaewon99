4ffd64d950f4a470a33222b9f6ea97f0101af0d5
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-13 17:26:07 +0900

    feat : 할인에 대한 혜택 내역을 생성하는 기능 추가

--- src/main/java/christmas/service/PromotionService.java
+++ src/main/java/christmas/service/PromotionService.java
@@ -9,12 +9,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PromotionService {
+    private static final int MINIMUM_GIVE_MENU_MONEY = 120_000;
+    private static final int CHAMPAGNE_PRICE = 25_000;
+    private static final int DISCOUNT_MONEY_NOT_EXIST_NUMBER = 0;
     private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance();
     private final OrderStore orderStore = new OrderStore();
     private final BenefitDetails benefitDetails = new BenefitDetails();
-
     private Customer customer;
     private int beforeTotalMoney;
+    private int dDayDiscount;
+    private int weekdayDiscount;
+    private int weekendDiscount;
+    private int specialDiscount;
+    private int totalBenefitMoney;
 
     public void createVisitDate(String inputDate) {
         customer = new Customer(inputDate);
@@ -26,12 +33,49 @@ public class PromotionService {
 
     public void checkMinimumTotalMoney() {
         if (isMinimumTotalMoney()) {
-
+            discountStatus();
         }
     }
 
-    public boolean isMinimumTotalMoney() {
-        return benefitDetails.minimumTotalMoney(beforeTotalMoney);
+    private void discountStatus() {
+        createdDayDiscount();
+        createWeekdayDiscount();
+        createWeekendDiscount();
+        createSpecialDiscount();
+        createTotalBenefitMoney();
+    }
+
+    private void createdDayDiscount() {
+        dDayDiscount = benefitDetails.calculatedDayDiscount(getVisitDate());
+    }
+
+    private void createWeekdayDiscount() {
+        if (benefitDetails.checkWeekday(getVisitDate())) {
+            for (int i = 0; i < getMenuSize(); i++) {
+                weekdayDiscount = benefitDetails.calculatedWeekdayDiscount(getMenuStore().get(i), getMenuCountStore().get(i));
+            }
+        }
+    }
+
+    private void createWeekendDiscount() {
+        if (benefitDetails.checkWeekend(getVisitDate())) {
+            for (int i = 0; i < getMenuSize(); i++) {
+                weekendDiscount = benefitDetails.calculatedWeekendDiscount(getMenuStore().get(i), getMenuCountStore().get(i));
+            }
+        }
+    }
+
+    private void createSpecialDiscount() {
+        specialDiscount = benefitDetails.calculateSpecialDiscount(getVisitDate());
+    }
+
+    private void createTotalBenefitMoney() {
+        if (isBeforeTotalMoney()) {
+            totalBenefitMoney = sumTotalBenefitMoney() + CHAMPAGNE_PRICE;
+        }
+        if (!isBeforeTotalMoney()) {
+            totalBenefitMoney = sumTotalBenefitMoney();
+        }
     }
 
     public String createBeforeTotalMoney() {
@@ -39,6 +83,66 @@ public class PromotionService {
             beforeTotalMoney += findMenuName(getMenuStore().get(i), getMenuCountStore().get(i));
         }
         return formatDiscount(beforeTotalMoney);
+    }
+
+    public boolean isGiveMenu() {
+        return beforeTotalMoney >= MINIMUM_GIVE_MENU_MONEY;
+    }
+
+    public boolean isMinimumTotalMoney() {
+        return benefitDetails.minimumTotalMoney(beforeTotalMoney);
+    }
+
+    public int sumTotalBenefitMoney() {
+        return totalBenefitMoney = dDayDiscount + weekdayDiscount + weekendDiscount + specialDiscount;
+    }
+
+    public boolean isBeforeTotalMoney() {
+        return beforeTotalMoney >= MINIMUM_GIVE_MENU_MONEY;
+    }
+
+    public String getEventDiscount() {
+        return formatDiscount(CHAMPAGNE_PRICE);
+    }
+
+    public String getdDayDiscount() {
+        return formatDiscount(dDayDiscount);
+    }
+
+    public String getWeekdayDiscount() {
+        return formatDiscount(weekdayDiscount);
+    }
+
+    public String getWeekendDiscount() {
+        return formatDiscount(weekendDiscount);
+    }
+
+    public String getSpecialDiscount() {
+        return formatDiscount(specialDiscount);
+    }
+
+    public boolean isdDayDiscountDiscountZero() {
+        return isDiscountZero(dDayDiscount);
+    }
+
+    public boolean isWeekDayDiscountZero() {
+        return isDiscountZero(weekdayDiscount);
+    }
+
+    public boolean isWeekendDiscountZero() {
+        return isDiscountZero(weekendDiscount);
+    }
+
+    public boolean isSpecialDiscountZero() {
+        return isDiscountZero(specialDiscount);
+    }
+
+    public boolean isTotalBenefitMoneyZero() {
+        return isDiscountZero(totalBenefitMoney);
+    }
+
+    public int getVisitDate() {
+        return customer.getVisitDate();
     }
 
     public int getMenuSize() {
@@ -62,5 +166,9 @@ public class PromotionService {
 
     private String formatDiscount(int discount) {
         return NUMBER_FORMAT.format(discount);
+    }
+
+    private boolean isDiscountZero(int discount) {
+        return discount != DISCOUNT_MONEY_NOT_EXIST_NUMBER;
     }
 }
