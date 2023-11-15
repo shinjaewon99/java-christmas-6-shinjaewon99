baaeb0491784528456891c04e9a2b78604582d8a
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-15 09:21:24 +0900

    feat : 음료만 주문 시, 주문할 수 없는 예외 기능 추가

--- src/main/java/christmas/domain/OrderStore.java
+++ src/main/java/christmas/domain/OrderStore.java
@@ -1,6 +1,7 @@
 package christmas.domain;
 
 import christmas.menu.Order;
+import christmas.menu.OrderType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,13 +12,17 @@ import java.util.regex.Pattern;
 public class OrderStore {
     private static final String INPUT_BLANK_EXCEPTION_MESSAGE = "[ERROR] 공백 없이 입력해야 합니다. 다시 입력해 주세요.";
     private static final String MAX_MENU_COUNT_EXCEPTION_MESSAGE = "[ERROR] 메뉴는 한번에 최대 20개까지만 가능합니다. 다시 입력해 주세요.";
+    private static final String MENU_ONLY_DRINK_TYPE_EXCEPTION_MESSAGE = "[ERROR] 음료만 주문 시, 주문할 수 없습니다. 다시 입력해 주세요.";
     private static final String MENU_EXCEPTION_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String MENU_TYPE_DRINK = "DRINK";
     private static final int MINIMUM_MENU_COUNT = 1;
+    private static final int MENU_TYPE_STORE_ONLY_SIZE_NUMBER = 1;
     private static final int TOTAL_MENU_COUNT_RESET_NUMBER = 0;
     private static final int LIMIT_MAX_MENU_COUNT_NUMBER = 20;
     private static final Pattern NUMBER_MATCH = Pattern.compile("^[0-9]*$");
     private final List<String> menuStore = new ArrayList<>();
     private final List<Integer> menuCountStore = new ArrayList<>();
+    private final List<String> menuTypeStore = new ArrayList<>();
     private int totalMenuCount;
 
     public void createMenuOrder(String inputMenu) {
@@ -29,6 +34,7 @@ public class OrderStore {
             validateMenu(menuDetails);
             validateMenuCount(menuDetails);
         }
+        validateMenuOnlyDrink();
         validateMaxOrderMenuCount();
         validateMenuDuplicate(menuStore);
     }
@@ -71,6 +77,34 @@ public class OrderStore {
     private void validateMenuCount(int menuCount) {
         if (!(menuCount >= MINIMUM_MENU_COUNT))
             throw new IllegalArgumentException(MENU_EXCEPTION_MESSAGE);
+    }
+
+    private void validateMenuOnlyDrink() {
+        createMenuTypeStore();
+        for (String menuType : menuTypeStore) {
+            validateMenuTypeStore(menuType);
+        }
+    }
+
+    private void validateMenuTypeStore(String menuType) {
+        if (menuTypeStore.size() == MENU_TYPE_STORE_ONLY_SIZE_NUMBER && menuType.equals(MENU_TYPE_DRINK)) {
+            menuTypeStore.clear();
+            throw new IllegalArgumentException(MENU_ONLY_DRINK_TYPE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void createMenuTypeStore() {
+        for (String menu : menuStore) {
+            addMenuTypeStore(menu);
+        }
+    }
+
+    private void addMenuTypeStore(String menu) {
+        for (OrderType orderType : OrderType.values()) {
+            if (orderType.getName().equals(menu)) {
+                menuTypeStore.add(orderType.getMenuType());
+            }
+        }
     }
 
     private void validateMaxOrderMenuCount() {
