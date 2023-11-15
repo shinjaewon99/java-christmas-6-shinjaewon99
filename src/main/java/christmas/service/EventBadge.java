9a2619dfa6b29d377f65e5839176efb2cb294d43
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-15 11:29:43 +0900

    refactor : eventBadge 필드 제거

--- src/main/java/christmas/service/EventBadge.java
+++ src/main/java/christmas/service/EventBadge.java
@@ -8,21 +8,17 @@ public class EventBadge {
     private static final String TREE_BADGE = "트리";
     private static final String START_BADGE = "별";
     private static final String NOTHING_BADGE = "없음";
-    private String eventBadge;
 
     public String calculateEventBadge(int totalBenefitMoney) {
         if (totalBenefitMoney >= MINIMUM_SANTA_BADGE_NUMBER) {
-            eventBadge = SANTA_BADGE;
+            return SANTA_BADGE;
         }
-        if (totalBenefitMoney >= MINIMUM_TREE_BADGE_NUMBER && totalBenefitMoney < MINIMUM_SANTA_BADGE_NUMBER) {
-            eventBadge = TREE_BADGE;
+        if (totalBenefitMoney >= MINIMUM_TREE_BADGE_NUMBER) {
+            return TREE_BADGE;
         }
-        if (totalBenefitMoney >= MINIMUM_START_BADGE_NUMBER && totalBenefitMoney < MINIMUM_TREE_BADGE_NUMBER) {
-            eventBadge = START_BADGE;
+        if (totalBenefitMoney >= MINIMUM_START_BADGE_NUMBER) {
+            return START_BADGE;
         }
-        if (totalBenefitMoney < MINIMUM_START_BADGE_NUMBER) {
-            eventBadge = NOTHING_BADGE;
-        }
-        return eventBadge;
+        return NOTHING_BADGE;
     }
 }
